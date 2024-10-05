e060a432b479dc5391c6904d85d4f79da9948b20
binhnguyen159 <mr.peace19999@gmail.com>
2024-10-05 14:07:33 +0700

    update pom file

--- starter/catpoint-parent/Image/src/main/java/module-info.java
+++ starter/catpoint-parent/Image/src/main/java/module-info.java
@@ -1,11 +1,10 @@
 module com.udacity.catpoint.image {
-//    requires org.slf4j;
-    requires software.amazon.awssdk.core;
-    requires software.amazon.awssdk.auth;
-    requires software.amazon.awssdk.services.rekognition;
-    requires java.desktop;
-    requires software.amazon.awssdk.regions;
-    requires org.slf4j;
+        requires software.amazon.awssdk.auth;
+        requires software.amazon.awssdk.core;
+        requires software.amazon.awssdk.regions;
+        requires software.amazon.awssdk.services.rekognition;
+        requires java.desktop;
+        requires org.slf4j;
 
-    exports com.udacity.catpoint.image;
+        exports com.udacity.catpoint.image;
 }
