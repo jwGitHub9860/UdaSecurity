4f8eaa19705c4a626b193ddb40d2a6de1f96dd2e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-31 00:16:50 -0400

    refactor: Refactoring production code
    Change comment for code that exports "net.miginfocom.swing.MigLayout" package inside "module-info.java" in "GUI" child module to correctly explain what code does

--- starter/catpoint-parent/GUI/src/module-info.java
+++ starter/catpoint-parent/GUI/src/module-info.java
@@ -1,4 +1,4 @@
 module GUI {
-    // Exports "net.miginfocom.swing.MigLayout" Package inside "GUI" child module
+    // Exports "net.miginfocom.swing.MigLayout" Package to Allow other Modules in "GUI" Child Module Access to "net.miginfocom.swing.MigLayout" Package
     exports net.miginfocom.swing.MigLayout;
 }
