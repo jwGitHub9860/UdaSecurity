3f12d97323dc101aa7779de922d019d60b67a53d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-31 00:54:17 -0400

    refactor: Refactoring production code
    Add code that exports "java.awt.image" package inside "module-info.java" in "Image" child module to allow other modules in "Image" child module access to "java.awt.image" package

--- starter/catpoint-parent/Image/src/module-info.java
+++ starter/catpoint-parent/Image/src/module-info.java
@@ -1,4 +1,7 @@
 module Image {
+    // Exports "java.awt.image" Package to Allow other Modules in "Image" Child Module Access to "java.awt.image" Package
+    exports java.awt.image;
+
     // Exports "software.amazon.awssdk" Package to Allow other Modules in "Image" Child Module Access to "software.amazon.awssdk" Package
     exports software.amazon.awssdk;
 }
