d874fffcf564ece733bbf397815a9e5992e4e27f
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-31 01:34:17 -0400

    refactor: Refactoring production code
    Add code that exports "com.google.common" package inside "module-info.java" in "Security" child module to allow other modules in "Security" child module access to "com.google.common" package

--- starter/catpoint-parent/Security/src/module-info.java
+++ starter/catpoint-parent/Security/src/module-info.java
@@ -1,1 +1,4 @@
-module Security {}
+module Security {
+    // Exports "com.google.common" Package to Allow other Modules in "Security" Child Module Access to "com.google.common" Package
+    exports com.google.common;
+}
