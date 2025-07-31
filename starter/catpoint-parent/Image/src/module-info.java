58308d210b8b24c23433a746ab268f53de974935
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-31 00:08:47 -0400

    refactor: Refactoring production code
    Add code that exports "software.amazon.awssdk" package inside "module-info.java" in "Image" child module to allow other modules in "Image" child module access to "software.amazon.awssdk" package

--- starter/catpoint-parent/Image/src/module-info.java
+++ starter/catpoint-parent/Image/src/module-info.java
@@ -1,1 +1,4 @@
-module Image {}
+module Image {
+    // Exports "software.amazon.awssdk" Package to Allow other Modules in "Image" Child Module Access to "software.amazon.awssdk" Package
+    exports software.amazon.awssdk;
+}
