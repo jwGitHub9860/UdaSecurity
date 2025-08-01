ced4b8ecf60d58a0604b71c9c78a92c971d3c5ab
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-31 21:51:30 -0400

    refactor: Refactoring production code
    Add comment for code that exports "com.udacity.catpoint.service" package inside "module-info.java" in "Security" child module to explain what files access java files inside "Security" child module

--- starter/catpoint-parent/Security/src/module-info.java
+++ starter/catpoint-parent/Security/src/module-info.java
@@ -1,6 +1,6 @@
 module Security {
     // Exports "com.udacity.catpoint.service" package to Allow OTHER Modules to Access Java Files INSIDE "Security" Child Module
-    exports com.udacity.catpoint.service;
+    exports com.udacity.catpoint.service; // "CatpointGui.java", "ControlPanel.java", "DisplayPanel.java", "ImagePanel.java", and "SensorPanel.java" Use "com.udacity.catpoint.service" Package
 
     // Requires ALL Files in "Security" Child Module TO HAVE ACCESS TO "com.google.common" Package
     requires com.google.common;
