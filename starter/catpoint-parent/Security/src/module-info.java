db530a4b9a75a8668d54dce104064975d02fa04a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-31 21:47:56 -0400

    refactor: Refactoring production code
    Add code that exports "com.udacity.catpoint.service" package inside "module-info.java" in "Security" child module to allow other modules to access java files inside "Security" child module

--- starter/catpoint-parent/Security/src/module-info.java
+++ starter/catpoint-parent/Security/src/module-info.java
@@ -1,4 +1,7 @@
 module Security {
+    // Exports "com.udacity.catpoint.service" package to Allow OTHER Modules to Access Java Files INSIDE "Security" Child Module
+    exports com.udacity.catpoint.service;
+
     // Requires ALL Files in "Security" Child Module TO HAVE ACCESS TO "com.google.common" Package
     requires com.google.common;
 }
