f6acf7fd8746b6fbf0443e377489e2ac424dc481
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-31 22:26:23 -0400

    refactor: Refactoring production code
    Replace "exports" with "requires" inside "module-info.java" in "Image" child module to have all files in "Image" child module require "java.awt.image" and "software.amazon.awssdk" packages

--- starter/catpoint-parent/Image/src/module-info.java
+++ starter/catpoint-parent/Image/src/module-info.java
@@ -1,7 +1,7 @@
 module Image {
-    // Exports "java.awt.image" Package to Allow other Modules in "Image" Child Module Access to "java.awt.image" Package
-    exports java.awt.image;
+    // Requires ALL Files in "Image" Child Module TO HAVE ACCESS TO "java.awt.image" Package
+    requires java.awt.image;
 
-    // Exports "software.amazon.awssdk" Package to Allow other Modules in "Image" Child Module Access to "software.amazon.awssdk" Package
-    exports software.amazon.awssdk;
+    // Requires ALL Files in "Image" Child Module TO HAVE ACCESS TO "software.amazon.awssdk" Package
+    requires software.amazon.awssdk;
 }
