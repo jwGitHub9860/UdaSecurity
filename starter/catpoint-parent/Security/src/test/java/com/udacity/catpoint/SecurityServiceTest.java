bdc63f214547568373861c5d6a5bb3a0ba408624
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-08-01 00:54:43 -0400

    refactor: Refactoring production code
    Delete new line between "public class SecurityServiceTest" and "extends TestCase" inside "SecurityServiceTest.java" java file in "test" directory in "Security" child module to decrease amount of code lines in project

--- starter/catpoint-parent/Security/src/test/java/com/udacity/catpoint/SecurityServiceTest.java
+++ starter/catpoint-parent/Security/src/test/java/com/udacity/catpoint/SecurityServiceTest.java
@@ -7,8 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class SecurityServiceTest
-    extends TestCase
+public class SecurityServiceTest extends TestCase
 {
     /**
      * Create the test case
