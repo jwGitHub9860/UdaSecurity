74023c0aa26db60541007270789b4d6a02673528
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-31 22:14:41 -0400

    refactor: Refactoring production code
    Add code that exports "com.udacity.catpoint.data" package inside "module-info.java" in "Image" child module to allow other modules to access java files inside "data" package in "Security" child module

--- starter/catpoint-parent/Security/src/module-info.java
+++ starter/catpoint-parent/Security/src/module-info.java
@@ -1,4 +1,7 @@
 module Security {
+    // Exports "com.udacity.catpoint.data" package to Allow OTHER Modules to Access Java Files INSIDE "data" Package in "Security" Child Module
+    exports com.udacity.catpoint.data; // "CatpointGui.java", "ControlPanel.java", "DisplayPanel.java", "ImagePanel.java", and "SensorPanel.java" Use "com.udacity.catpoint.data" Package
+
     // Exports "com.udacity.catpoint.service" package to Allow OTHER Modules to Access Java Files INSIDE "service" Package in "Security" Child Module
     exports com.udacity.catpoint.service; // "CatpointGui.java", "ControlPanel.java", "DisplayPanel.java", "ImagePanel.java", and "SensorPanel.java" Use "com.udacity.catpoint.service" Package
 
